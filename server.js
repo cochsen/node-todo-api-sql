7af792d77ffb945bbbd307f3eebe21c296464cc5
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2016-02-20 11:39:27 -0500

    underscore functions

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var  app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -19,11 +20,7 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id);
-  var matchedTodo;
-  todos.forEach(function (todo) {
-    if (todoId === todo.id)
-      matchedTodo = todo;
-  });
+  var matchedTodo =   _.findWhere(todos, {id: todoId});
   if (matchedTodo) {
     res.json(matchedTodo);
   }
@@ -34,13 +31,17 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-  var body = req.body;
-  // add id field
+  // pick just the required data
+  var body = _.pick(req.body, 'description', 'completed');
+  // validate input
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    return res.status(400).send();
+  }
+  // set body.description to the trimmed value
+  body.description = body.description.trim();
   body.id = todoNextId++;
-  // push data onto todos array
   todos.push(body);
   res.json(body);
-
 });
 
 app.listen(PORT, function () {
