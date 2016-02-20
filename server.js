e4813cdf703478f75e3b926d9efac25e51bf7d2a
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2016-02-19 20:53:24 -0500

    method to receive POSTs

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var  app = express();
 var PORT = process.env.PORT || 3000;
-// example data
-var todos = [{
-  id: 1,
-  description: 'Go to Reading Terminal Market',
-  completed: false
-},  {
-  id: 2,
-  description: 'Have lunch with Mom',
-  completed: false
-},  {
-  id: 3,
-  description: 'Get Phillies tickets',
-  completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
   res.send('Todo API Root');
@@ -39,6 +30,17 @@ app.get('/todos/:id', function (req, res) {
   else {
     res.status(404).send();
   }
+});
+
+// POST /todos
+app.post('/todos', function (req, res) {
+  var body = req.body;
+  // add id field
+  body.id = todoNextId++;
+  // push data onto todos array
+  todos.push(body);
+  res.json(body);
+
 });
 
 app.listen(PORT, function () {
