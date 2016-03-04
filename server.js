bda9133b2bd16e83c0668b2f3a3012c38cc445fa
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2016-03-04 14:30:12 -0500

    add sqlite

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var _ = require('underscore')
+var db = require('./db.js')
 var  app = express()
 var PORT = process.env.PORT || 3000
 var todos = []
@@ -99,6 +100,8 @@ app.put('/todos/:id', function (req, res) {
   res.json(matchedTodo)
 })
 
-app.listen(PORT, function () {
-  console.log('Express listening on port ' + PORT + '!')
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log('Express listening on port ' + PORT + '!')
+  })
 })
