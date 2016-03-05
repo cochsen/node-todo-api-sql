557ed30e4a5563640248675988e844537a664547
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2016-03-04 19:52:41 -0500

    sequelize GET /todos?completed= ?q=

--- server.js
+++ server.js
@@ -15,20 +15,24 @@ app.get('/', function (req, res) {
 
 // GET
 app.get('/todos', function (req, res) {
-  var queryParams = req.query
-  var filteredTodos = todos
-  // if /todos?completed=true
-  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true')
-    filteredTodos = _.where(filteredTodos, {completed: true})
-  // if /todos?completed=false
-  if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false')
-    filteredTodos = _.where(filteredTodos, {completed: false})
-  // filter todos by query string
-  if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-    filteredTodos = _.filter(filteredTodos, function (todo) { return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1 })
+  var query = req.query
+  var where = {}
+  if (query.hasOwnProperty('completed') && query.completed === 'true') {
+    where.completed = true;
   }
-
-  res.json(filteredTodos)
+  else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+    where.completed = false
+  }
+  if (query.hasOwnProperty('q') && query.q.length > 0) {
+    where.description = {
+      $like: '%' + query.q + '%'
+    }
+  }
+  db.todo.findAll({where: where}).then(function (todos) {
+    res.json(todos)
+  }, function (e) {
+    res.status(500).send()
+  })
 })
 
 // GET /todos/:id
