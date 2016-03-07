39a3a5db635c6a27f8d18728fe10b4cb6e348cbb
Chris Ochsenreither <christopher.ochsenreither@gmail.com>
2016-03-06 21:28:56 -0500

    added user model and POST /user

--- server.js
+++ server.js
@@ -62,6 +62,16 @@ app.post('/todos', function (req, res) {
   })
 })
 
+// POST /user
+app.post('/user', function (req, res) {
+  var body = _.pick(req.body, 'email', 'password')
+  db.user.create(body).then(function (user) {
+    res.json(user.toJSON())
+  }, function (e) {
+    res.status(400).json(e)
+  })
+})
+
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
   var todoId = parseInt(req.params.id)
